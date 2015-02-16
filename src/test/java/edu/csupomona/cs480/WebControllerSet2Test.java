cab7e59883b1e6891ab5d3b5cf91f88bdd2accc0
Mitchell Manqueros <manquerosmitchell@yahoo.com>
2015-02-15 21:19:46 -0800

    Commented out test delete

---
+++ src/test/java/edu/csupomona/cs480/WebControllerSet2Test.java
@@ -0,0 +1,39 @@
+package edu.csupomona.cs480;
+
+import static org.junit.Assert.*;
+
+import java.net.UnknownHostException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mongodb.DBObject;
+
+import edu.csupomona.cs480.controller.WebController;
+
+
+public class WebControllerSet2Test 
+{
+    
+	private WebController web;
+	
+	@Before
+	public void setup() throws UnknownHostException
+	{
+		web = new WebController();
+	}
+	/*
+	@Test
+	public void testDeleteList()
+	{   
+		boolean result = web.deleteList("54d6631c0fdcf8e36aa174d8");
+		assertTrue(result);
+	}
+	*/
+	@Test
+	public void testUserExists() throws UnknownHostException
+	{
+		boolean result = web.userExists("test123");
+		assertTrue(result);
+	}
+}
