5c9699e299c4aaf3f0f85416c991dec4596e81a9
siegelhorn <siegelhorn@gmail.com>
2015-02-17 23:33:18 -0800

    Implemented Signup page.     Something is wrong with MongoLab db, not accepting users.     Works fine with a local db.

---
+++ src/main/resources/static/js/signup.js
@@ -0,0 +1,31 @@
+/**
+ * Created by isaacsiegel on 2/17/15.
+ */
+
+
+
+function attemptSignup() {
+    var userName = $('#emailField').val();
+    var password = $('#passwordField').val();
+
+    $.ajax(
+        {
+            type : "POST",
+            url  : "/cs480/createUser/" + userName,
+            data : {
+                password: password
+            },
+            success : function(result) {
+                //console.log(result)
+                if(result == true)
+                    alert("You're awesome. Go sign in.")
+                else {
+                       alert("Username taken, try another.")
+                }
+
+            },
+            error: function (jqXHR, exception) {
+                alert("Error during signup.");
+            }
+        });
+}
