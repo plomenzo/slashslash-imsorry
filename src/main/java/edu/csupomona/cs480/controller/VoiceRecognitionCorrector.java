2294591bc4e9f6726f88adb38e34dd4e40dee65f
unknown <vincent@koeten.net>
2015-03-01 17:41:50 -0800

    Adding support for finding the VoiceRecognitionCorrections.txt file on the server.

--- src/main/java/edu/csupomona/cs480/controller/VoiceRecognitionCorrector.java
+++ src/main/java/edu/csupomona/cs480/controller/VoiceRecognitionCorrector.java
@@ -12,7 +12,8 @@ public class VoiceRecognitionCorrector
 	// uses incorrect as key and correct as value
 	private Hashtable<String, String> converter;
 	// Path to the text file containing the corrections
-	private static final String CORRECTIONS_FILE_PATH = "src/main/resources/static/documents/VoiceRecognitionCorrections.txt";
+	private static final String LOCAL_CORRECTIONS_FILE_PATH = "src/main/resources/static/documents/VoiceRecognitionCorrections.txt";
+	private static final String SERVER_CORRECTIONS_FILE_PATH = "http://ec2-54-201-132-163.us-west-2.compute.amazonaws.com/documents/VoiceRecognitionCorrections.txt";
 	
 	/**
      * Gets the instance of the VoiceRecognitionCorrector
@@ -35,7 +36,7 @@ public class VoiceRecognitionCorrector
 		converter = new Hashtable<String, String>();
 		try
 		{
-			File correctionsFile = new File(CORRECTIONS_FILE_PATH);
+			File correctionsFile = chooseExistingFile(LOCAL_CORRECTIONS_FILE_PATH, SERVER_CORRECTIONS_FILE_PATH);
 			if(correctionsFile.exists())
 			{
 				System.out.println("FILE FOUND FOR VRC");
@@ -61,6 +62,31 @@ public class VoiceRecognitionCorrector
 		{
 			System.out.println("Exception thrown in VoiceRecognitionCorrector: " + ex);
 		}
+	}
+	
+	/**
+     * Chooses which file exists and returns that file
+     * @param filePath1 the String of the first file path
+	 * @param filePath2 the String of the second file path
+     * @return the existing file, if both exists then it returns filePath1, if neither exists then returns null
+     */
+	private File chooseExistingFile(String filePath1, String filePath2)
+	{
+		File file1 = new File(filePath1);
+		if(file1.exists())
+		{
+			System.out.println("File path 1 exists: " + filePath1);
+			return file1;
+		}
+		File file2 = new File(filePath2);
+		if(file2.exists())
+		{
+			System.out.println("File path 2 exists: " + filePath2);
+			return file2;
+		}
+		// neither file exists so return null
+		System.out.println("Neither file path exists: " + filePath1 + " , " + filePath2);
+		return null;
 	}
 	
 	/**
